be341d89539838db7e028907cf2e7380d7d88e54
Matheus Rian <souzamatheusrian@gmail.com>
2021-03-01 16:44:57 -0300

    feat: add ListProvider in app.module ✨

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
-import { FilmesProvider } from '../providers/filmes/filmes';
+import { FilmesProvider } from '../providers/filmes/filmes.provider';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ListProvider } from '../providers/list/list.provider';
 @NgModule({
   declarations: [
     MyApp,
@@ -30,6 +31,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     SplashScreen,
     FilmesProvider,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ListProvider,
   ]
 })
 export class AppModule {}
