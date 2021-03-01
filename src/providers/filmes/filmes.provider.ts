b416519b06f1b52c9abe2409e5196ae42455fa7c
Matheus Rian <souzamatheusrian@gmail.com>
2021-03-01 16:53:28 -0300

    feat: add FilmesProvider✨

---
+++ src/providers/filmes/filmes.provider.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment as ENV } from "../../environments/environment";
+import { FilmesInt } from "../../interfaces/filmes.interface";
+
+@Injectable()
+export class FilmesProvider {
+  constructor(public http: HttpClient) {}
+
+  readData() {
+    return this.http.get<FilmesInt>(`${ENV.API_BASE}discover/tv?api_key=${ENV.API_KEY}&language=pt-BR`)
+  }
+
+  movieTopRated() {
+    return this.http.get<FilmesInt>(`${ENV.API_BASE}movie/top_rated?api_key=${ENV.API_KEY}&language=pt-BR`)
+  }
+
+  movieRecommendation() {
+    return this.http.get<FilmesInt>(`${ENV.API_BASE}trending/all/week?api_key=${ENV.API_KEY}&language=pt-BR`)
+  }
+
+  movieAction() {
+    return this.http.get<FilmesInt>(`${ENV.API_BASE}discover/tv?with_genres=28&api_key=${ENV.API_KEY}&language=pt-BR`)
+  }
+
+  movieComedy() {
+    return this.http.get<FilmesInt>(`${ENV.API_BASE}discover/tv?with_genres=35&api_key=${ENV.API_KEY}&language=pt-BR`)
+  }
+
+  movieTerror() {
+    return this.http.get<FilmesInt>(`${ENV.API_BASE}discover/tv?with_genres=27&api_key=${ENV.API_KEY}&language=pt-BR`)
+  }
+
+  movieRomance() {
+    return this.http.get<FilmesInt>(`${ENV.API_BASE}discover/tv?with_genres=10749&api_key=${ENV.API_KEY}&language=pt-BR`)
+  }
+
+  movieDocumentary() {
+    return this.http.get<FilmesInt>(`${ENV.API_BASE}discover/tv?with_genres=99&api_key=${ENV.API_KEY}&language=pt-BR`)
+  }
+
+}
